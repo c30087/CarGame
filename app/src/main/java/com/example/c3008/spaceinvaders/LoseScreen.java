8ae204f784c93685dbb5949618e789d3c39c3bf8
c30087 <c30087@student.ghchs.com>
2017-07-28 14:24:32 -0700

    added files

---
+++ app/src/main/java/com/example/c3008/spaceinvaders/LoseScreen.java
@@ -0,0 +1,22 @@
+package com.example.c3008.spaceinvaders;
+
+/**
+ * Created by c3008 on 7/28/2017.
+ */
+
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.widget.Button;
+
+public class LoseScreen extends Activity {
+
+    Button button;
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_space_invaders2);
+    }
+
+}
